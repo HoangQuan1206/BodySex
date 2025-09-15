226bb8e823cb986f3f68b0934ac43340d4a04020
HoangQuan1206 <161178302+HoangQuan1206@users.noreply.github.com>
2025-09-15 22:32:05 +0700

    Update KaitunBFBanana.lua

--- KaitunBFBanana.lua
+++ KaitunBFBanana.lua
@@ -1,87 +1,104 @@
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
+
 getgenv().Key = "fce29705ab1438631ae8d43c"
-    getgenv().SettingFarm ={
-        ["Hide UI"] = true,
-        ["Reset Teleport"] = {
-            ["Enabled"] = true,
-            ["Delay Reset"] = 3,
-            ["Item Dont Reset"] = {
-                ["Fruit"] = {
+getgenv().SettingFarm = {
+    ["Hide UI"] = true,
+    ["Reset Teleport"] = {
+        ["Enabled"] = true,
+        ["Delay Reset"] = 3,
+        ["Item Dont Reset"] = {
+            ["Fruit"] = {
+                ["Enabled"] = true,
+                ["All Fruit"] = true, 
+                ["Select Fruit"] = {
                     ["Enabled"] = true,
-                    ["All Fruit"] = true, 
-                    ["Select Fruit"] = {
-                        ["Enabled"] = true,
-                        ["Fruit"] = {"Magma-Magma", "Quake-Quake", "Buddha-Buddha", "Love-Love", "Spider-Spider", "Sound-Sound", "Phoenix-Phoenix", "Portal-Portal", "Rumble-Rumble", "Pain-Pain", "Gravity-Gravity", "Dough-Dough", "Shadow-Shadow", "Venom-Venom", "Control-Control", "Gas-Gas", "Spirit-Spirit", "Dragon-Dragon", "Mammoth-Mammoth", "Leopard-Leopard", "East Dragon-East Dragon", "West Dragon-West Dragon"
-},
+                    ["Fruit"] = {
+                        "Magma-Magma", "Quake-Quake", "Buddha-Buddha", "Love-Love", 
+                        "Spider-Spider", "Sound-Sound", "Phoenix-Phoenix", "Portal-Portal", 
+                        "Rumble-Rumble", "Pain-Pain", "Gravity-Gravity", "Dough-Dough", 
+                        "Shadow-Shadow", "Venom-Venom", "Control-Control", "Gas-Gas", 
+                        "Spirit-Spirit", "Dragon-Dragon", "Mammoth-Mammoth", "Leopard-Leopard", 
+                        "East Dragon-East Dragon", "West Dragon-West Dragon"
                     },
                 },
             },
         },
-        ["White Screen"] = false,
-        ["Lock Fps"] = {
-            ["Enabled"] = true,
-            ["FPS"] = 120,
+    },
+    ["White Screen"] = false,
+    ["Lock Fps"] = {
+        ["Enabled"] = true,
+        ["FPS"] = 120,
+    },
+    ["Get Items"] = {
+        ["Saber"] = true,
+        ["Godhuman"] = true,
+        ["Skull Guitar"] = true,
+        ["Valkyrie Helm"] = true,
+        ["Mirror Fractal"] = true,
+        ["Cursed Dual Katana"] = true,
+        ["Upgrade Race V2-V3"] = true,
+        ["Auto Pull Lever"] = true,
+        ["Shark Anchor"] = true,
+    },
+    ["Get Rare Items"] = {
+        ["Rengoku"] = true,
+        ["Dragon Trident"] = true, 
+        ["Pole (1st Form)"] = true,
+        ["Gravity Blade"] = true,
+    },
+    ["Farm Fragments"] = {
+        ["Enabled"] = true,
+        ["Fragment"] = 50000,
+    },
+    ["Auto Chat"] = {
+        ["Enabled"] = false,
+        ["Text"] = " anhhub kaitun ontop : danhng. ",
+    },
+    ["Auto Summon Rip Indra"] = true,
+    ["Select Hop"] = {
+        ["Hop Server If Have Player Near"] = false, 
+        ["Hop Find Rip Indra Get Valkyrie Helm or Get Tushita"] = true, 
+        ["Hop Find Dough King Get Mirror Fractal"] = true,
+        ["Hop Find Raids Castle [CDK]"] = false,
+        ["Hop Find Cake Queen [CDK]"] = true,
+        ["Hop Find Soul Reaper [CDK]"] = true,
+        ["Hop Find Darkbeard [SG]"] = true,
+        ["Hop Find Mirage [ Pull Lever ]"] = true,
+    },
+    ["Farm Mastery"] = {
+        ["Melee"] = true,
+        ["Sword"] = false,
+    },
+    ["Buy Haki"] = {
+        ["Enhancement"] = true,
+        ["Skyjump"] = true,
+        ["Flash Step"] = true,
+        ["Observation"] = true,
+    },
+    ["Sniper Fruit Shop"] = {
+        ["Enabled"] = true,
+        ["Fruit"] = {
+            "Rumble-Rumble", "Pain-Pain", "Gravity-Gravity", "Dough-Dough", 
+            "Shadow-Shadow", "Venom-Venom", "Control-Control", "Gas-Gas", 
+            "Spirit-Spirit", "Dragon-Dragon", "Mammoth-Mammoth", "Leopard-Leopard", 
+            "East Dragon-East Dragon", "West Dragon-West Dragon"
         },
-        ["Get Items"] = {
-            ["Saber"] = true,
-            ["Godhuman"] =  true,
-            ["Skull Guitar"] = true,
-            ["Valkyrie Helm"] = true,
-            ["Mirror Fractal"] = true,
-            ["Cursed Dual Katana"] = true,
-            ["Upgrade Race V2-V3"] = true,
-            ["Auto Pull Lever"] = true,
-            ["Shark Anchor"] = true, --- if have cdk,sg,godhuman
-        },
-        ["Get Rare Items"] = {
-            ["Rengoku"] = true,
-            ["Dragon Trident"] = true, 
-            ["Pole (1st Form)"] = true,
-            ["Gravity Blade"]  = true,
-        },
-        ["Farm Fragments"] = {
-            ["Enabled"]  = true,
-            ["Fragment"] = 50000,
-        },
-        ["Auto Chat"] = {
-            ["Enabled"] = false,
-            ["Text"] = " anhhub kaitun ontop : danhng. ",
-        },
-        ["Auto Summon Rip Indra"] = true, --- auto buy haki and craft haki legendary 
-        ["Select Hop"] = { -- 70% will have it
-            ["Hop Server If Have Player Near"] = false, 
-            ["Hop Find Rip Indra Get Valkyrie Helm or Get Tushita"] = true, 
-            ["Hop Find Dough King Get Mirror Fractal"] = true,
-            ["Hop Find Raids Castle [CDK]"] = false,
-            ["Hop Find Cake Queen [CDK]"] = true,
-            ["Hop Find Soul Reaper [CDK]"] = true,
-            ["Hop Find Darkbeard [SG]"] = true,
-            ["Hop Find Mirage [ Pull Lever ]"] = true,
-        },
-        ["Farm Mastery"] = {
-            ["Melee"] = true,
-            ["Sword"] = false,
-        },
-        ["Buy Haki"] = {
-            ["Enhancement"] = true,
-            ["Skyjump"] = true,
-            ["Flash Step"] = true,
-            ["Observation"] = true,
-        },
-        ["Sniper Fruit Shop"] = {
-            ["Enabled"] = true, -- Auto Buy Fruit in Shop Mirage and Normal
-            ["Fruit"] = {"Rumble-Rumble", "Pain-Pain", "Gravity-Gravity", "Dough-Dough", "Shadow-Shadow", "Venom-Venom", "Control-Control", "Gas-Gas", "Spirit-Spirit", "Dragon-Dragon", "Mammoth-Mammoth", "Leopard-Leopard", "East Dragon-East Dragon", "West Dragon-West Dragon"
-},
-        },
-	["Get Race"] = {
-	     ["Cyborg"] = true
-	     ["Ghoul"] = false
-	     ["Draco"] = false
-	},
-        ["Lock Fruit"] = {"Rumble-Rumble", "Pain-Pain", "Gravity-Gravity", "Dough-Dough", "Shadow-Shadow", "Venom-Venom", "Control-Control", "Gas-Gas", "Spirit-Spirit", "Dragon-Dragon", "Mammoth-Mammoth", "Leopard-Leopard", "East Dragon-East Dragon", "West Dragon-West Dragon"},
-        ["Webhook"] = {
-            ["Enabled"] = true,
-            ["WebhookUrl"] = "https://discord.com/api/webhooks/1411194628006219776/16nlkh3LpQruj5D25URDUIsABPZnVTNa44YoycfwM-wIMNParpTuf7lHzb2r4wmhX8JH",
-        }
+    },
+    ["Get Race"] = {
+        ["Cyborg"] = true,
+        ["Ghoul"] = false,
+        ["Draco"] = false,
+    },
+    ["Lock Fruit"] = {
+        "Rumble-Rumble", "Pain-Pain", "Gravity-Gravity", "Dough-Dough", 
+        "Shadow-Shadow", "Venom-Venom", "Control-Control", "Gas-Gas", 
+        "Spirit-Spirit", "Dragon-Dragon", "Mammoth-Mammoth", "Leopard-Leopard", 
+        "East Dragon-East Dragon", "West Dragon-West Dragon"
+    },
+    ["Webhook"] = {
+        ["Enabled"] = true,
+        ["WebhookUrl"] = "https://discord.com/api/webhooks/1411194628006219776/16nlkh3LpQruj5D25URDUIsABPZnVTNa44YoycfwM-wIMNParpTuf7lHzb2r4wmhX8JH",
     }
+}
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BananaCat-kaitunBF.lua"))()
